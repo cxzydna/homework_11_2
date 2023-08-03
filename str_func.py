38a7ffe13f44155f441cff8eff50c8302cfc55ad
cxzydna <blackwhite901@mail.ru>
2023-08-03 17:50:07 +0300

    a

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def foo(a: str) -> str:
+    """
+    aaa
+    :param a:
+    :return:
+    """
     return a.upper()
+
+
+def foo_2(a: str) -> str:
+    """
+    aaa
+    :param a:
+    :return:
+    """
+    return a.title()
